0209247483929688829082252aa4daec014a09bf
James Cathcart <cathcart.jm@gmail.com>
2020-11-29 01:31:46 -0700

    WIP - getting current

---
+++ ch2/search/feed.go
@@ -0,0 +1,31 @@
+package search
+
+import (
+	"encoding/json"
+	"os"
+)
+
+const dataFile = "data/data.json"
+
+type Feed struct {
+	Name string `json:"site"`
+	URI string `json:"link"`
+	Type string `json:"type"`
+}
+
+
+// retrieve, read, and unmarshal the feed data
+func RetrieveFeeds() ([]*Feed, error) {
+
+	file, err := os.Open(dataFile)
+	if err != nil {
+		return nil, err
+	}
+
+	defer file.Close()
+
+	var feeds []*Feed
+	err = json.NewDecoder(file).Decode(&feeds)
+
+	return feeds, err
+}
